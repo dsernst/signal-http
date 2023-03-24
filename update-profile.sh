5d4b88a550713908ff423439341885a72c4f5c86
David Ernst <git@dsernst.com>
2023-03-24 17:12:27 -0500

    Script to update profile name and avatar

---
+++ update-profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Import config variables
+. ./config.sh
+
+./signal-cli updateProfile --name "$DISPLAY_NAME"
+
+if [ -f "./avatar.png" ]; then
+  ./signal-cli updateProfile --avatar ./avatar.png
+else
+  echo "No ./avatar.png found, skipping"
+fi
