65e138e9f9593011b567dca41561d88ed3f26ab6
David Ernst <git@dsernst.com>
2023-03-25 23:21:39 -0500

    Don't escape [ ]

--- sending.ts
+++ sending.ts
@@ -11,12 +11,10 @@ app.post('/send', (req, res) => {
 
   // Is it ready to send out?
   if (to) {
-    const escaped = (message || '')
-      .replace(/\\/g, '\\\\')
-      // .replace(/'/g, "'\\''") Not needed
-      .replace(/"/g, '\\"')
-      .replace(/\[/g, '\\[')
-      .replace(/\]/g, '\\]')
+    const escaped = (message || '').replace(/\\/g, '\\\\').replace(/"/g, '\\"')
+    // .replace(/'/g, "'\\''") Not needed
+    // .replace(/\[/g, '\\[')
+    // .replace(/\]/g, '\\]')
     let command = `${signal} --dbus send -m "${escaped}" ${toGroup ? `-g ${toGroup}` : to}`
     if (!debug) command += ' 2> /dev/null'
     try {
