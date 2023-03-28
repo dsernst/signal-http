0b5f9dcc222923f420799b792d05aa015efdaef5
David Ernst <git@dsernst.com>
2023-03-28 13:55:58 -0500

    Use local ./cli-config folder for data

--- path-to-signal.ts
+++ path-to-signal.ts
@@ -1,3 +1,4 @@
 // Export shared path to signal-cli
 import { join } from 'path'
-export const signal = join(__dirname + '/../node_modules/.bin/signal-cli') + (process.env.SIGNAL_CLI_FLAGS || '')
+
+export const signal = join(__dirname + '/../node_modules/.bin/signal-cli') + ' --config ./cli-config'
