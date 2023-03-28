16d6f4003a4ff4c2321a4bca9dbd9bfbaef92206
David Ernst <git@dsernst.com>
2023-03-28 00:38:41 -0500

    process.env.SIGNAL_CLI_FLAGS

--- path-to-signal.ts
+++ path-to-signal.ts
@@ -1,3 +1,3 @@
 // Export shared path to signal-cli
 import { join } from 'path'
-export const signal = join(__dirname + '/../node_modules/.bin/signal-cli')
+export const signal = join(__dirname + '/../node_modules/.bin/signal-cli') + (process.env.SIGNAL_CLI_FLAGS || '')
